495560a5e3933af770b9f1a22896cb73430be243
SergeYudkin <87810884+SergeYudkin@users.noreply.github.com>
2022-06-12 14:27:23 +0300

    Урок 1  BottomAppBar + FAB + NavigationDrawer

--- app/src/main/java/com/example/coursematerial/pictureoftheday/PictureOfTheDayFragment.kt
+++ app/src/main/java/com/example/coursematerial/pictureoftheday/PictureOfTheDayFragment.kt
@@ -15,15 +15,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import coil.load
+import com.example.coursematerial.R
 import com.example.coursematerial.view.MainActivity
 import com.example.coursematerial.databinding.FragmentPictureOfTheDayBinding
 import com.example.coursematerial.viewmodel.AppState
 import com.example.coursematerial.viewmodel.PictureOfTheDayViewModel
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import retrofit2.http.Url
 import java.util.*
 
 class PictureOfTheDayFragment : Fragment() {
+
+
+    var isMain = true
 
     private var _binding : FragmentPictureOfTheDayBinding? = null
     private val binding : FragmentPictureOfTheDayBinding
@@ -51,11 +56,67 @@ class PictureOfTheDayFragment : Fragment() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.app_bar_fav -> Toast.makeText(context, "Favourite", Toast.LENGTH_SHORT).show()
+            R.id.app_bar_settings -> Toast.makeText(context, "Settings", Toast.LENGTH_SHORT).show()
+            android.R.id.home-> {
+                BottomNavigationDrawerFragment().show(requireActivity().supportFragmentManager,"")
+            }
+
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+        actionBar()
         request()
         clickWiki()
+        BSB ()
+        FAB()
 
+
+    }
+
+
+    fun FAB(){
+        binding.fab.setOnClickListener {
+            isMain = !isMain
+            if (!isMain) {
+                binding.bottomAppBar.navigationIcon = null
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+                binding.fab.setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_back_fab))
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar_other)
+            } else {
+                binding.bottomAppBar.navigationIcon = ContextCompat.getDrawable(requireContext(),
+                    R.drawable.ic_hamburger_menu_bottom_bar)
+
+                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+                binding.fab.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_plus_fab
+                    )
+                )
+                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar)
+            }
+
+        }
+    }
+
+    private fun actionBar(){
+        (requireActivity() as MainActivity).setSupportActionBar(binding.bottomAppBar)
+    }
+
+    private fun BSB (){
         val params = (binding.lifeHack.bottomSheetContainer.layoutParams as CoordinatorLayout.LayoutParams)
         val behavior =  params.behavior as BottomSheetBehavior
         behavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
@@ -64,12 +125,12 @@ class PictureOfTheDayFragment : Fragment() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 when(newState){
 
-                 /*   BottomSheetBehavior.STATE_COLLAPSED -> { TODO() }
-                    BottomSheetBehavior.STATE_DRAGGING -> { TODO() }
-                    BottomSheetBehavior.STATE_EXPANDED -> { TODO() }
-                    BottomSheetBehavior.STATE_HALF_EXPANDED -> { TODO() }
-                    BottomSheetBehavior.STATE_HIDDEN -> { TODO() }
-                    BottomSheetBehavior.STATE_SETTLING -> { TODO() }*/
+                    /*   BottomSheetBehavior.STATE_COLLAPSED -> { TODO() }
+                       BottomSheetBehavior.STATE_DRAGGING -> { TODO() }
+                       BottomSheetBehavior.STATE_EXPANDED -> { TODO() }
+                       BottomSheetBehavior.STATE_HALF_EXPANDED -> { TODO() }
+                       BottomSheetBehavior.STATE_HIDDEN -> { TODO() }
+                       BottomSheetBehavior.STATE_SETTLING -> { TODO() }*/
                 }
             }
 
