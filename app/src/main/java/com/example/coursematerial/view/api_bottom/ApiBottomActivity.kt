f00468fedeee0f01723688a9492815a06497474e
SergeYudkin <87810884+SergeYudkin@users.noreply.github.com>
2022-06-21 22:06:36 +0300

    Урок 3 BottomNavigationView

---
+++ app/src/main/java/com/example/coursematerial/view/api_bottom/ApiBottomActivity.kt
@@ -0,0 +1,58 @@
+package com.example.coursematerial.view.api_bottom
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.coursematerial.R
+import com.example.coursematerial.databinding.ActivityApiBinding
+import com.example.coursematerial.databinding.ActivityApiBottomBinding
+import com.example.coursematerial.view.api.*
+import com.google.android.material.badge.BadgeDrawable
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+
+class ApiBottomActivity :AppCompatActivity() {
+
+    lateinit var binding: ActivityApiBottomBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityApiBottomBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+        setupNavigation ()
+
+
+    }
+
+    private fun setupNavigation (){
+        binding.bottomNavigationView.selectedItemId = R.id.action_bottom_view_mars
+
+      val badge =   binding.bottomNavigationView.getOrCreateBadge(R.id.action_bottom_view_system)
+        badge.number= 1000000
+        badge.maxCharacterCount= 8  // колличество выводимых знаков
+        badge.badgeGravity = BadgeDrawable.BOTTOM_START  // отображение снизу  слева. По умолчанию сверху справа.
+        badge.clearNumber() // удаляет все значения, остаётся пустое поле
+       // binding.bottomNavigationView.removeBadge(R.id.action_bottom_view_system)  // удаление badge
+    }
+
+    private fun init(){
+        binding.bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.action_bottom_view_earth->{
+                    supportFragmentManager.beginTransaction().replace(R.id.container,EarthFragment()).commit()
+                    true
+                }
+
+                R.id.action_bottom_view_mars->{
+                    supportFragmentManager.beginTransaction().replace(R.id.container, MarsFragment()).commit()
+                    true
+                }
+                R.id.action_bottom_view_system->{
+                    supportFragmentManager.beginTransaction().replace(R.id.container, SystemFragment()).commit()
+                    true
+                }
+            }
+            true
+        }
+    }
+}
